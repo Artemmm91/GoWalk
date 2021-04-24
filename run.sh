851695ded8fbde6eee533818a31942546554f62d
Artem Mustafin <24766926+Artemmm91@users.noreply.github.com>
2021-04-25 00:20:59 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ python3 -m venv ./venv
 source venv/bin/activate
 pip3 install -r ../requirements.txt
 cd ..
-python3 manage.py makemigraions
+python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver
