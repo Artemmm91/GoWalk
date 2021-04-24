3c7d67e8697511db0357e3b31bae6dabbe058fd2
Artemmm91 <artem.mustafin.2002@mail.ru>
2021-04-25 00:05:01 +0300

    deleted for pull request

--- walkapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Walk, Option, Vote, Tag, Comment
-
-
-admin.site.register(Walk)
-admin.site.register(Option)
-admin.site.register(Vote)
-admin.site.register(Tag)
-admin.site.register(Comment)
--- walkapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WalkappConfig(AppConfig):
-    name = 'walkapp'
